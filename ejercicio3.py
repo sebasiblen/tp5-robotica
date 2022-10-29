9db88072c7f59fd76db78a473312a36e58aa7024
sebasiblen <61847500+sebasiblen@users.noreply.github.com>
2022-10-29 04:13:47 -0300

    cubo

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('vehiculo.jpg')
+img = cv2.imread('cubo.jpg')
 imgHSV= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 blueB = np.array([100,50,50], np.uint8)
